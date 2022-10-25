8c0308a0469c51fc7daab685476bea08971606e1
GiacomoBelliniStudent <115079590+GiacomoBelliniStudent@users.noreply.github.com>
2022-10-25 16:54:48 +0200

    Add files via upload
    update stop at ten matches awk script

--- CommandLine.sh
+++ CommandLine.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-awk '{
+limit=0;
+awk -v limit=$limit '{
   if (length($8)>100){
       {print $4};
+      limit++;
   }
+  if (limit==10)
+    exit;
 }' instagram_posts.csv > profileList.txt
-awk '{print $1} NR==10{exit}' profileList.txt > firstTenPosts.txt
-profileList='firstTenPosts.txt'
+profileList='profileList.txt'
 profiles='instagram_profiles.csv'
 head -n1 instagram_profiles.csv | tr , '\n' > result.txt
 grep -f <(tr ',' '\n' < "${profileList}") "${profiles}" >> result.txt
-rm firstTenPosts.txt
 rm profileList.txt
 while read -r line; do echo $line; printf "\n"; done < result.txt
